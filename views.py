84740a479c038371368097c843da3597793372c8
jasensteve10 <messianezapfack@gmail.com>
2016-08-24 17:47:01 +0100

    changes in views.py

---
+++ views.py
@@ -0,0 +1,16 @@
+sfrom django.shortcuts import render
+
+
+# def home(request):
+#      context = {}
+#      template = "home.html"
+#      return render(request, template, context)
+    
+
+
+#def home2(request):
+   #context = {}
+   #template = "home.html2"
+   #return render(request, template, context)
+
+   
